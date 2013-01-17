349f0c26a7f76790a29f80e671e5fc5ddd2b226f
Grisha <grisha@grisha.(none)>
2013-01-17 08:45:10 -0800

    adding some libraries

--- public/js/gregkerzhner.js
+++ public/js/gregkerzhner.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-	(function($){
-
 	window.Intro = Backbone.Model.extend({});
 	window.IntroView = Backbone.View.extend({
 		template: _.template($("#intro-template").html()),
@@ -13,12 +11,46 @@ $(document).ready(function(){
 		},
 		render: function(){
 			$(this.el).html(this.template(this.model.toJSON()));
+			for(var i = 0; i<16;i++){
+				var square = function(ctx) {
+			    	ctx.fillStyle = "red";
+			    	ctx.fillRect(Math.floor((Math.random()*300)+1), Math.floor((Math.random()*300)+1), 80, 80);
+				}
+				$g('introCanvas').size(400, 600).add(square);
+			}
+
+			$g('introCanvas').size(400, 600).add({
+			    theta: 0, 
+			    draw: function(ctx) {
+			        ctx.save();
+			        ctx.translate(50, 50);
+			        ctx.rotate(this.theta);
+			        ctx.fillStyle = "#ada";
+			        ctx.fillRect(-32, -32, 64, 64);
+			        ctx.restore();
+			        this.theta += Math.PI / 120;
+			    }
+			}).play(16);
+
             return this;
 		}
-	})
-})(jQuery);
-	var intro = new Intro()
-	var introView = new IntroView({model: intro});
-	$("#container").html(introView.render().el)
+	});
+	
+	window.Gregkerzhner = Backbone.Router.extend({
+	routes: {
+	    '': 'home',	   
+	},
 
-})
+	initialize: function() {
+		intro = new Intro();
+	    this.introView = new IntroView({model: intro});		
+	 },
+	 home: function(){
+	 	$("canvas").html(this.introView.render().el)
+
+	 }}
+	 );	
+	window.App = new Gregkerzhner();
+    Backbone.history.start();
+});
+
