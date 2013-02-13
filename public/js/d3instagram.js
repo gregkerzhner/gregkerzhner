b19a62d5f4b9b1e8f87cbb27d8b66a68ee4fca1b
guidoprincess <guidoprincess@gmail.com>
2013-02-13 11:50:11 -0800

    fixing timer

--- public/js/d3instagram.js
+++ public/js/d3instagram.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+    $(document).ready(function(){
     window.animations[2] =  Backbone.View.extend({
         template: _.template($("#instagram-map-template").html()),
         className:  "intro",
@@ -36,6 +36,7 @@ $(document).ready(function(){
                         .style("stroke", "rgb(6,120,155)");
             });
             $(".instagram-search-container .search").click(function(){
+                dotCounter = 0;
                 svg.selectAll("circle").remove();
                 searchInstagram("https://api.instagram.com/v1/tags/"+$(".search-term").val()+"/media/recent?access_token=306225576.f59def8.ddd35b2913c945d8b6f0e88f840c9944&callback=?");
             });
@@ -43,7 +44,7 @@ $(document).ready(function(){
                 $.getJSON(url, function(data){
                     for(var i = 0; i<data.data.length;i++){
                         var hashtag = data.data[i];
-                        if(hashtag.location){
+                        if(hashtag.location && hashtag.location.longitude && hashtag.location.latitude){
                             dotCounter++;
                             var points = d3.geo.mercator().scale(900).translate([500, 500])([hashtag.location.longitude,hashtag.location.latitude])
                             svg.append("svg:circle")
